c9a629013ee7a905edd0dc23f8090978bab6de74
pabloparejo <parejo.p@gmail.com>
2014-05-05 16:02:12 +0200

    Heroku deploy

--- shop/wsgi.py
+++ shop/wsgi.py
@@ -9,4 +9,6 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shop.settings")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
